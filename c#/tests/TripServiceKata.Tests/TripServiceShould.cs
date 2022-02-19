d7808bd213d27d248647e64067048ee18a7273d4
evaderrama <evalderrama@domingoalonsogroup.com>
2022-02-19 20:14:25 +0000

    ✅Add first test ( characterization test )

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,6 +1,16 @@
-﻿namespace TripServiceKata.Tests
+﻿using Xunit;
+
+namespace TripServiceKata.Tests
 {
     public class TripServiceShould
     {
+        [Fact]
+        public void failTest()
+        {
+            var tripService = new TripService();
+            tripService.GetTripsByUser(null);
+
+            Assert.True(false);
+        }
     }
 }
